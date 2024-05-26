b24737cbe19eafbcb5b1bb9ab1b1c23c4d6dcfcf
lomotb-xleba <145037143+lomotb-xleba@users.noreply.github.com>
2024-05-26 22:09:21 +0800

    Add files via upload

--- obj/Debug/net6.0-windows/hw_Content.g.i.cs
+++ obj/Debug/net6.0-windows/hw_Content.g.i.cs
@@ -8,7 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("back.mp3")]
 [assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("wrong.mp3")]
 
 
